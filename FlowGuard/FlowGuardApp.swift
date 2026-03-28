2308b891b8bc1e0b17be2a8138773847a93c62ac
uvays shakhmurzaev <shahmurzaev.u@rwb.ru>
2026-03-28 18:41:45 +0300

    Initial Commit

---
+++ FlowGuard/FlowGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowGuardApp.swift
+//  FlowGuard
+//
+//  Created by uvays on 28.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
